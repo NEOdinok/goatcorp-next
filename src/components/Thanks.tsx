116d3bed7a0e0742b6dec9089368efdb5ed706b2
goshamartynovich <goshamartynovich@gmail.com>
2024-12-21 15:35:37 +0300

    clear cart on thanks

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { LoadingGoat } from "@/components";
+import { useCart } from "@/hooks";
 import { useQuery } from "@tanstack/react-query";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
-import { LoadingServisex } from "./ui/loading-servisex";
-
 export const Thanks: React.FC = () => {
   const searchParams = useSearchParams();
   const orderId = searchParams.get("orderId");
+  const { clearCart } = useCart();
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders", orderId],
@@ -27,23 +28,16 @@ export const Thanks: React.FC = () => {
   if (isLoading)
     return (
       <div className="flex justify-center items-center grow">
-        <LoadingServisex />
+        <LoadingGoat />
       </div>
     );
 
   const orderPaid = data?.orders?.[0]?.status === "paid";
 
+  if (orderPaid) clearCart();
+
   return orderPaid ? <SuccessAfterPayment /> : <ErrorAfterPayment />;
 };
-
-// const SuccessAfterPayment = () => (
-//   <div className="flex grow items-center justify-center gap-2 sm:gap-4 py-2 px-2 sm:py-0">
-//     <p>Спасибо за твой заказ друг!</p>
-//     <Link className="underline hover:text-primary" href="/shop">
-//       В магазин
-//     </Link>
-//   </div>
-// );
 
 const SuccessAfterPayment = () => {
   return (
@@ -60,8 +54,6 @@ const SuccessAfterPayment = () => {
     </motion.div>
   );
 };
-
-export default SuccessAfterPayment;
 
 const ErrorAfterPayment = () => (
   <div className="flex flex-col grow items-center justify-center gap-2 sm:gap-4 py-2 px-2 sm:py-0">
